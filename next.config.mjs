60e25df470469cf50ab070bb4c16e4799fa7f7be
wnteh0077 <wnteh0077@gmail.com>
2024-05-17 03:02:08 +0800

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
     /** @type {import('next').NextConfig} */
     const nextConfig = {
         images: {
-            domains: ['tarumt-ecommerce-s3.s3.amazonaws.com', 'tarumt-ecommerce.s3.amazonaws.com'],
+            remotePatterns: [
+                {
+                    protocol: "http",
+                    hostname: "tarumt-ecommerce-s3.s3.amazonaws.com"
+                }
+            ]
         },
     };
     
